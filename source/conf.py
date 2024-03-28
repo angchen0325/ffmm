d6fa052b20d7d497a04d5352356cc0f69c7867b6
Ang Chen <chenang@outlook.com>
2024-03-28 13:48:11 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,14 +1,33 @@
+# -- Path setup --------------------------------------------------------------
+import os
+import re
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'ffmm'
 copyright = '2024, Ang Chen'
-author = 'Ang Chen'
+release = find_version('..', 'ffmm', '__init__.py')
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
